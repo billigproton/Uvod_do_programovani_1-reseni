456ce0a018528912ca8fa0c16e791e746bafb4c3
billigproton <danrabien@seznam.cz>
2024-06-24 20:14:38 +0200

    mala uprava

--- vstup_a_vystup_cviceni/jmeno.py
+++ vstup_a_vystup_cviceni/jmeno.py
@@ -2,19 +2,15 @@
 
 jmeno_prijmeni = input("Zadejte Vaše jméno a příjmení: ")
 
-vek = int(input("Zadejte váš věk: "))
+vek = input("Zadejte váš věk: ")
+
+print(jmeno_prijmeni + ", " + vek)
 
  # další způsob zápisu
  
 print(jmeno_prijmeni, vek, sep=", ")
 
- # kdyby jste nechtěli převádět věk na číslo
-
-vek = input("Zadejte váš věk: ")
-
-print(jmeno_prijmeni + ", " + vek)
-
- # formátovaný řetězec
+ # formátovaný řetězec - ideální zápis :)
 
 vek = int(input("Zadejte váš věk:"))
 
